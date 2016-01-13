0ec033b7a523d12deb68eff8128e4fff30608d7f
Cedric Gerber <cedrigerber94@gmail.com;git config --global user.email cedrigerber94@gmail.com>
2016-01-13 09:41:37 +0100

    avancement du serveur REST

--- Server.py
+++ Server.py
@@ -3,10 +3,30 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-@app.route('/<chemin>')
-def index(chemin=None):
-    return "Hello {}!".format(chemin)
+# @app.route('/')
+# @app.route('/<chemin>')
+# def index(chemin=None):
+#     return "Hello {}!".format(chemin)
+# @app.route('/channels/obstacles')
+# def obstacle():
+#     pass
+# @app.route('/channels/newpos/<x><y><teta>')#rajoute methode post
+# def newpos(x,y,teta):
+#     pass
+# @app.route('/channels/mesuredpos')
+# def mesuredpos():
+#     pass
+
+c = {'obstacles':[2,3]}
+@app.route('/channels')
+@app.route('/channels/<channel_name>',methods=['GET'])
+def f(channel_name=None):
+    if channel_name == None:
+        #print all channels
+        for val in c.iteritems():
+            return "{}".format(val)
+    else:
+        return "{}".format(c[channel_name])
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=False)
+    app.run(port=5000, debug=True)
