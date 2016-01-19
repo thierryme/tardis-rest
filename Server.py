d277aa1c6c4901d1499c9cd28e28bc1321f48adf
Cedric Gerber <cedrigerber94@gmail.com;git config --global user.email cedrigerber94@gmail.com>
2016-01-19 16:52:55 +0100

    amélioration du serveur

--- Server.py
+++ Server.py
@@ -4,7 +4,7 @@ import serial
 import json
 import time
 
-from threading import Thread
+from threading import Thread, Lock
 
 from flask import Flask, request, jsonify, abort, render_template
 
@@ -24,8 +24,8 @@ app = Flask(__name__)
 # def mesuredpos():
 #     pass
 
-c = {'obstacles': [2, 3]}
-
+c = {'obstacles': [0, 0],'new_pos':[0,0,0],'mesured_pos':[0,0,0], 'nb_spots':0}
+mutex = Lock()
 
 @app.route('/')
 def client():
@@ -47,19 +47,26 @@ def f(channel_name=None):
     else:
         if not request.json:
             abort(400)
+        mutex.acquire()
         c[channel_name] = request.json[channel_name]
+        mutex.release()
         return jsonify({channel_name: c[channel_name]}), 200
 
 
 class SerialManager(Thread):
     """Communication trough serial port"""
     def __init__(self):
-        super(SerialManager, self).__init__()
+        Thread.__init__(self)
+        self.setDaemon(True)#pour killer le thread avec ctrl + C
 
     def run(self):
         ser_connected = "No serial"
-
+        #dat = c['new_pos']
+        #print dat
+        #print "dans le thread"
         while True:
+            #dat = c['new_pos']
+            #print dat
             try:
                 ser = serial.Serial('/dev/ttyUSB1', 115200)  # open serial port
                 ser_connected = ser.name
@@ -68,31 +75,29 @@ class SerialManager(Thread):
                 try:
                     while True:
 
-                            line = ser.readline()
-                            try:
-                                data = json.loads(line)
-                                print(data)
-                            except ValueError:
-                                print("Non-valid")
+	                    line = ser.readline()
+	                    try:
+	                        data = json.loads(line)
+	                        print(data)
+	                    except ValueError:
+	                        print("Non-valid")
 
-                            data_to_send = {}
-                            data_to_send['new_pos'] = [200, 200, 0]
+	                    data_to_send = {}
+	                    data_to_send['new_pos'] = [200, 200, 0]
+	                    data_to_send['new_pos'] = c['new_pos']
+	                    #print dat
 
-                            ser.write(json.dumps(data_to_send)+'\n')
+	                    ser.write(json.dumps(data_to_send)+'\n')
                 finally:
-                        ser.close()
-                        print("{} disconnected".format(ser_connected))
-
+	                ser.close()
+	                print("{} disconnected".format(ser_connected))
             except serial.serialutil.SerialException:
-
                 time.sleep(3)
+                print "dans le thread"
 
 
 if __name__ == '__main__':
 
     serialManager = SerialManager()
-
-    serialManager.run()
+    serialManager.start()
     app.run(host='0.0.0.0', port=5000, debug=True)
-
-    serialManager.join()
