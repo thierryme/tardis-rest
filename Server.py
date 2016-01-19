7af8c6774f9dcafbd57da47bb6027e035a626816
namas <gostmasterys@gmail.com>
2016-01-19 17:24:08 +0100

    comm with displacing module, mutex added

--- Server.py
+++ Server.py
@@ -24,8 +24,9 @@ app = Flask(__name__)
 # def mesuredpos():
 #     pass
 
-c = {'obstacles': [0, 0],'new_pos':[0,0,0],'mesured_pos':[0,0,0], 'nb_spots':0}
+channels = {'obstacles': [0, 0], 'new_pos': [0, 0, 0], 'mesured_pos': [0, 0, 0], 'nb_spots': 0}
 mutex = Lock()
+
 
 @app.route('/')
 def client():
@@ -38,59 +39,69 @@ def f(channel_name=None):
     if request.method == 'GET':
         if channel_name is None:
             #print all channels
-            return jsonify(c)
+            return jsonify(channels)
 
         else:
-            return jsonify({channel_name: c[channel_name]})
+            return jsonify({channel_name: channels[channel_name]})
 
     #si methode POST
     else:
         if not request.json:
             abort(400)
-        mutex.acquire()
-        c[channel_name] = request.json[channel_name]
-        mutex.release()
-        return jsonify({channel_name: c[channel_name]}), 200
+
+        with mutex:
+            channels[channel_name] = request.json[channel_name]
+
+        return jsonify({channel_name: channels[channel_name]}), 200
 
 
 class SerialManager(Thread):
     """Communication trough serial port"""
     def __init__(self):
-        Thread.__init__(self)
-        self.setDaemon(True)#pour killer le thread avec ctrl + C
+        Thread.__init__(self, serial='/dev/ttyUSB1')
+
+        self.serial_name = serial
+        self.setDaemon(True)  # pour killer le thread avec ctrl + C
 
     def run(self):
         ser_connected = "No serial"
-        #dat = c['new_pos']
+        #dat = channels['new_pos']
         #print dat
         #print "dans le thread"
         while True:
-            #dat = c['new_pos']
+            #dat = channels['new_pos']
             #print dat
             try:
-                ser = serial.Serial('/dev/ttyUSB1', 115200)  # open serial port
+                ser = serial.Serial(self.serial_name, 115200)  # open serial port
                 ser_connected = ser.name
                 print("{} connected".format(ser_connected))         # check which port was really used
 
                 try:
                     while True:
 
-	                    line = ser.readline()
-	                    try:
-	                        data = json.loads(line)
-	                        print(data)
-	                    except ValueError:
-	                        print("Non-valid")
+                        line = ser.readline()
+                        try:
+                            data = json.loads(line)
 
-	                    data_to_send = {}
-	                    data_to_send['new_pos'] = [200, 200, 0]
-	                    data_to_send['new_pos'] = c['new_pos']
-	                    #print dat
+                            for key, item in data.items:
+                                with mutex:
+                                    channels[key] = item
 
-	                    ser.write(json.dumps(data_to_send)+'\n')
+                                print(channels[key])
+
+                        except ValueError:
+                            print("Non-valid")
+
+                        data_to_send = {}
+                        #data_to_send['new_pos'] = [200, 200, 0]
+
+                        with mutex:
+                            data_to_send['new_pos'] = channels['new_pos']
+
+                        ser.write(json.dumps(data_to_send)+'\n')
                 finally:
-	                ser.close()
-	                print("{} disconnected".format(ser_connected))
+                    ser.close()
+                    print("{} disconnected".format(ser_connected))
             except serial.serialutil.SerialException:
                 time.sleep(3)
                 print "dans le thread"
@@ -98,6 +109,6 @@ class SerialManager(Thread):
 
 if __name__ == '__main__':
 
-    serialManager = SerialManager()
+    serialManager = SerialManager('/dev/ttyACM0006')
     serialManager.start()
     app.run(host='0.0.0.0', port=5000, debug=True)
